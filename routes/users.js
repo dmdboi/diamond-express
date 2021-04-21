4e55bcdedbc9641ebb5196b5063262be1fd6b579
dmdboi <hello@maxdiamond.co.uk>
2021-04-21 20:22:22 +0100

    undo

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,27 @@
-var express = require("express");
-var router = express.Router();
-var authentication = require("../middleware/auth");
-var userController = require("../controllers/users");
-var wrapper = function (fn) { return function (req, res, next) {
-    return Promise.resolve(fn(req, res, next).catch(next));
-}; };
+const express = require("express");
+const router = express.Router();
+
+const authentication = require("../middleware/auth");
+
+const userController = require("../controllers/users");
+
+const wrapper = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next).catch(next));
+
 router.post("/register", wrapper(userController.register));
 router.post("/login", wrapper(userController.login));
+
 router.use(authentication.check);
+
 router.get("/me", wrapper(userController.me));
+
 //JWT Verification on login
 router.get("/verify", wrapper(userController.verify));
+
 //Update Account Route
 router.post("/update", wrapper(userController.updateAccount));
+
 //Delete Account Route
 router.post("/delete", wrapper(userController.deleteAccount));
+
 module.exports = router;
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import app from './app';
-const PORT = process.env.PORT;
-
-app.listen(3000, () => {
-  //Depending on setup, skip console.log()
-  console.log(`Express server running on http://localhost:3000`);
-});
